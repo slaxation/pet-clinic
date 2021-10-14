9c29abccf6e7474e7624eaa6a6d2d55e33927edb
Peter Bornstein <zmaster.allstar@gmail.com>
2021-10-14 17:57:23 +0200

    Added vet list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/slax/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/slax/petclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package slax.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import slax.petclinic.services.VetService;
+
 
 @Controller
 public class VetController {
+    private final VetService vetService;
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public VetController(VetService ownerService) {
+        this.vetService = ownerService;
+    }
+
+    @RequestMapping({"/vets", "/", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
+
     }
 }
