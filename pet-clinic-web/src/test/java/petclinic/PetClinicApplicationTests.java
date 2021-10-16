9d41d63c1138a9b78d8a71b4a156be99219e7b94
Peter Bornstein <zmaster.allstar@gmail.com>
2021-10-16 19:36:27 +0200

    Added wro4j plugin and config from Spring Pet Clinic. Closes #24.

--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,13 +1,18 @@
 package petclinic;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class PetClinicApplicationTests {
+public class PetClinicApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
