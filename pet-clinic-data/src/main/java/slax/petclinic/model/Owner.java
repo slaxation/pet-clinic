65ead73af30a8815dcfa6e38ce85ddf2d10f51a8
Peter Bornstein <zmaster.allstar@gmail.com>
2021-10-20 16:14:31 +0200

    Updated owner with JPA entity mappings. Closes #36.

--- pet-clinic-data/src/main/java/slax/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/slax/petclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package slax.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @Column(name = "address")
     private String address;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>() ;
 
     public String getCity() {
