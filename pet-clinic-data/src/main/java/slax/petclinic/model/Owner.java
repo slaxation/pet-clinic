01eabb73cbca6cd44046788d81aca15d7e195c87
Peter Bornstein <zmaster.allstar@gmail.com>
2021-10-21 20:38:14 +0200

    Added Project Lombok. Closes #51.

--- pet-clinic-data/src/main/java/slax/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/slax/petclinic/model/Owner.java
@@ -1,12 +1,29 @@
 package slax.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
+
+    @Column(name = "address")
+    private String address;
 
     @Column(name = "city")
     private String city;
@@ -14,41 +31,8 @@ public class Owner extends Person {
     @Column(name = "telephone")
     private String telephone;
 
-    @Column(name = "address")
-    private String address;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
