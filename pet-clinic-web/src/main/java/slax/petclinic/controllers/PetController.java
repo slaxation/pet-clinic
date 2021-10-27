ace830a879f1b728ed36fccf4ac1b9a3d32679cc
Peter Bornstein <zmaster.allstar@gmail.com>
2021-10-27 21:29:22 +0200

    fixing parse. Closes #62.

--- pet-clinic-web/src/main/java/slax/petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/slax/petclinic/controllers/PetController.java
@@ -52,6 +52,7 @@ public class PetController {
     public String initCreationForm(Owner owner, Model model) {
         Pet pet = new Pet();
         owner.getPets().add(pet);
+        pet.setOwner(owner);
         model.addAttribute("pet", pet);
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
     }
@@ -61,6 +62,7 @@ public class PetController {
         if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(), true) != null){
             result.rejectValue("name", "duplicate", "already exists");
         }
+        pet.setOwner(owner);
         owner.getPets().add(pet);
         if (result.hasErrors()) {
             model.put("pet", pet);
@@ -85,7 +87,7 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
-            owner.getPets().add(pet);
+            pet.setOwner(owner);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
